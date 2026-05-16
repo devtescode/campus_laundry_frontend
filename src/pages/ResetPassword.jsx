6b2069bfc9cde0c6e11ca61e7a22736f7255c64f
devtescode <teslimagboola09@gmail.com>
2026-05-16 22:19:26 +0100

    update code

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { API_URLS } from "../components/utils/apiConfig";
+import Swal from "sweetalert2";
 
 const ResetPassword = () => {
   const { token } = useParams();
